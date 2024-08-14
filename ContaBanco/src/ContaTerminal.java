e26a572e5e2c9afdcd5e1de649fe13ec23c0478c
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-14 17:38:55 -0300

    Add try_resources

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste");
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+            //TODO: regras de negocio.
+        } catch (Exception e) {
+            // TODO: regras para exception
+        }      
+        
     }
 }
