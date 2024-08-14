b08af6b3274f1729fa3e2c94ac9281816934f05c
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-14 18:09:09 -0300

    Add regras de negocio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,29 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        try (Scanner scanner = new Scanner(System.in)) {
-            //TODO: regras de negocio.
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+
+            System.out.println("Por favor, digite o número da Conta !");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o número da Agência !");
+            String agencia = scanner.next();
+
+            System.out.println("Por favor, digite o seu Nome !");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Por favor, digite o Saldo !");
+            double saldo = scanner.nextDouble();
+
+            System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+                        
         } catch (Exception e) {
-            // TODO: regras para exception
+            
+            System.out.println("\n\nErro ao criar conta: " + e.getMessage());
+
         }      
         
     }
