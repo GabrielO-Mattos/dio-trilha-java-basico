9922ca747a2504a752594d63c43dd6a64c615059
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-15 16:10:38 -0300

    Finalizado regras de negocio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -8,6 +8,7 @@ public class Contador {
 		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
+        terminal.close();
 		
 		try {
 			//chamando o método contendo a lógica de contagem
@@ -15,12 +16,13 @@ public class Contador {
 		
 		}catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            System.out.println(exception);
 		}
 		
 	}
     
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
@@ -28,5 +30,11 @@ public class Contador {
 		
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
+
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + (i+1));
+        }
+
 	}
+
 }
