65b135c82439f8c95001792ad3042c2d33fab20d
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-18 11:27:31 -0300

    Update class Cliente

--- DesafioBancoDigital/src/App.java
+++ DesafioBancoDigital/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Cliente c1 = new Cliente("DIO", "123456789", "Juridica");
+        Cliente c2 = new Cliente("Gabriel", "123456789", "Fisica");
+
+        System.out.println(c1.getNome() + c1.getCnpj() + c1.getCpf());
+        System.out.println(c2.getNome() + c2.getCnpj() + c2.getCpf());
+
     }
 }
--- DesafioBancoDigital/src/Cliente.java
+++ DesafioBancoDigital/src/Cliente.java
@@ -1,3 +1,65 @@
+import java.time.LocalDate;
+
 public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String cnpj;
+    private LocalDate dataNascimentoDate;
+    private String telefone;
+    private String tipoDePessoa;
+
+    /* Get's and Set's */
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public LocalDate getDataNascimentoDate() {
+        return dataNascimentoDate;
+    }
+    public void setDataNascimentoDate(LocalDate dataNascimentoDate) {
+        this.dataNascimentoDate = dataNascimentoDate;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getTipoDePessoa() {
+        return tipoDePessoa;
+    }
+    public void setTipoDePessoa(String tipoDePessoa) {
+        this.tipoDePessoa = tipoDePessoa;
+    }
+    
+
+    /* Construtor */
+    public Cliente(String nome, String cpf_cnpj, String tipoDePessoa) {
+        this.nome = nome;
+        this.tipoDePessoa = tipoDePessoa;
+
+        if(tipoDePessoa.equals("Juridica")) {
+            this.cnpj = cpf_cnpj;
+        } else {
+            this.cpf = cpf_cnpj;
+        }
+    }
+
+
 
 }
