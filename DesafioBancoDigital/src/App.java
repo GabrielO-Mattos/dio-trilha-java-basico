449b69114d4c290278dfd05a01bcad16fb30e483
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-18 13:15:21 -0300

    "Added Banco class with methods to manage contas, and updated App class to use Banco"

--- DesafioBancoDigital/src/App.java
+++ DesafioBancoDigital/src/App.java
@@ -1,13 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Banco banco = new Banco();
+
         Cliente c1 = new Cliente("DIO", "111", "Juridica");
         Cliente c2 = new Cliente("Gabriel", "112", "Fisica");
 
         Conta cc1 = new ContaCorrente(c1);
         Conta cc2 = new ContaPoupanca(c2);
+        
+        banco.adicionarConta(cc1);
+        banco.adicionarConta(cc2);
+        banco.exibirSaldoGlobal();
 
-        cc1.extrato();
+        /*cc1.extrato();
         cc2.extrato();
         
         cc2.sacar(500, true);
@@ -22,7 +28,8 @@ public class App {
         cc2.extrato();
         cc2.transferir(cc1, 3000);
         cc1.extrato();
-        cc2.extrato();
+        cc2.extrato();*/
+
 
 
     }
--- DesafioBancoDigital/src/Banco.java
+++ DesafioBancoDigital/src/Banco.java
@@ -1,3 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Banco {
+    private Map<String, Conta> contas;
+    
+    public Banco() {
+        this.contas = new HashMap<>();
+    }
+
+    public void adicionarConta(Conta conta){
+        contas.put(Integer.toString(conta.getNumeroConta()), conta);
+    }
+
+    public void removerConta(String numeroConta) {
+        contas.remove(numeroConta);
+    }
+
+    public Conta buscarConta(String numeroConta) {
+        return contas.get(numeroConta);
+    }
+
+    public void exibirTodasAsContas() {
+        for (Conta conta : contas.values()) {
+            System.out.println(conta);
+        }
+    }
+
+    public void exibirSaldoGlobal(){
+        double total = 0;
+        for (Conta conta : contas.values()) {
+            total += conta.getSaldo();
+            }
+        System.out.println("O saldo global é: " + total);
+    }
 
 }
