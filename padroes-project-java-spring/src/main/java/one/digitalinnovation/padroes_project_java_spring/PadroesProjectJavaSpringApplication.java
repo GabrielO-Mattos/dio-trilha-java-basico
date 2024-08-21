81ac58ac6790138202c74decdf1472cbb06a6fd3
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 17:02:53 -0300

    "Created Java project using Spring Boot"

---
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/padroes_project_java_spring/PadroesProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.padroes_project_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjectJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjectJavaSpringApplication.class, args);
+	}
+
+}
---
+++ padroes-project-java-spring/src/test/java/one/digitalinnovation/padroes_project_java_spring/PadroesProjectJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.padroes_project_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjectJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
