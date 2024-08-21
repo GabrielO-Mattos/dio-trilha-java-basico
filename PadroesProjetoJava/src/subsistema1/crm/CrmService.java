33056bafca4f9d8887e1427444d770ecc6e9e234
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 11:58:40 -0300

    "Added imports and implemented Facede class to interact with CrmService and CepApi; updated CrmService to print client information."

--- PadroesProjetoJava/src/subsistema1/crm/CrmService.java
+++ PadroesProjetoJava/src/subsistema1/crm/CrmService.java
@@ -6,8 +6,12 @@ public class CrmService {
         super();
     }
 
-    public static void gravarCliente(String nomeCliente, String cep, String cidade, String estado){
+    public static void gravarCliente(String nome, String cep, String cidade, String estado){
         System.out.println("Cliente salvo no sistema CRM.");
+        System.out.println(nome);
+        System.out.println(cep);
+        System.out.println(cidade);
+        System.out.println(estado);
     }
 
 }
