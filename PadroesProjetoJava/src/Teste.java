eb112a6c98490a9e07a4fb3f03ee11fd8187fca9
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 11:38:59 -0300

    "Added Singleton pattern tests and implemented Strategy pattern with Robo and Comportamento classes."

--- PadroesProjetoJava/src/Teste.java
+++ PadroesProjetoJava/src/Teste.java
@@ -1,37 +1,61 @@
 import one.digitalinnovation.gof.singleton.SingletonEager;
 import one.digitalinnovation.gof.singleton.SingletonLazy;
 import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.ComportamentoOfensivo;
+import one.digitalinnovation.gof.strategy.Robo;
 
 public class Teste {
     public static void main(String[] args) throws Exception {
 
-        /* Chamando o metodo para verificar se a instancia foi pega corretamente */
+        // TESTE RELACIONADOS AO DESIGN PATTERN SINGLETON
+        
+        // Chamando o metodo para verificar se a instancia foi pega corretamente
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         
-        /* Chamando o metodo novamente para verificar se a mesma instancia foi atibuida */
+        // Chamando o metodo novamente para verificar se a mesma instancia foi atibuida
         lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
 
 
 
-        /* Chamando o metodo para verificar se a instancia foi pega corretamente */
+        // Chamando o metodo para verificar se a instancia foi pega corretamente
         SingletonEager eager = SingletonEager.getSingletonEager();
         System.out.println(eager);
         
-        /* Chamando o metodo novamente para verificar se a mesma instancia foi atibuida */
+        // Chamando o metodo novamente para verificar se a mesma instancia foi atibuida
         eager = SingletonEager.getSingletonEager();
         System.out.println(eager);
 
 
 
-        /* Chamando o metodo para verificar se a instancia foi pega corretamente */
+        // Chamando o metodo para verificar se a instancia foi pega corretamente
         SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
         
-        /* Chamando o metodo novamente para verificar se a mesma instancia foi atibuida */
+        // Chamando o metodo novamente para verificar se a mesma instancia foi atibuida
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
+
+
+        // TESTE RELACIONADOS AO DESIGN PATTERN STRATEGY
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento ofensivo = new ComportamentoOfensivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(ofensivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
 
     }
 }
