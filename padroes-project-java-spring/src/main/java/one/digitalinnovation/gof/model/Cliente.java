5b86176d0b45a73f4ceafb880cc1220bace619f4
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 18:29:06 -0300

    "Added JPA annotations and getters/setters to Cliente class"

--- padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -1,8 +1,43 @@
 package one.digitalinnovation.gof.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    @ManyToOne
+    private String endereco;
+
     
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+
+
+
 }
