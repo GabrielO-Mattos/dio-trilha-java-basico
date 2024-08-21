035be72667249e4d200bee661133886ca8561f2a
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 20:08:22 -0300

    "Added ViaCepService interface for external CEP lookup using GET method"

---
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.service;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import one.digitalinnovation.gof.model.Endereco;
+
+public interface ViaCepService {
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
