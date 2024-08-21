036ccb5cdcc6e7f7bab3e08b2bd50e698c573b88
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 11:54:06 -0300

    "Created classes for CRM and CEP subsystems: `CrmService` to handle CRM operations, `CepApi` for accessing and retrieving city and state information based on ZIP codes, and `Facade` to provide a unified interface for client migration."

---
+++ PadroesProjetoJava/src/one/digitalinnovation/gof/facade/Facede.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof.facade;
+
+public class Facede {
+
+    public void migrarCliente(String nome, String cep){
+        
+    }
+
+}
---
+++ PadroesProjetoJava/src/subsistema1/crm/CrmService.java
@@ -0,0 +1,13 @@
+package subsistema1.crm;
+
+public class CrmService {
+
+    private CrmService(){
+        super();
+    }
+
+    public static void gravarCliente(String nomeCliente, String cep, String cidade, String estado){
+        System.out.println("Cliente salvo no sistema CRM.");
+    }
+
+}
