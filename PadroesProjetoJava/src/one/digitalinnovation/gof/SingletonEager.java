c38054201380bd00388007c111f20cf7c7a43dc5
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 10:59:39 -0300

    "Added static instance and getter method to SingletonEager class"

--- PadroesProjetoJava/src/one/digitalinnovation/gof/SingletonEager.java
+++ PadroesProjetoJava/src/one/digitalinnovation/gof/SingletonEager.java
@@ -2,4 +2,14 @@ package one.digitalinnovation.gof;
 
 public class SingletonEager {
 
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getSingletonEager(){
+        return instancia;
+    }
+
 }
