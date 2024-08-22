9cec2e78b105a5836c52792cb6829531493562ea
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 22:38:14 -0300

    "Refactored Cliente.java: reformatted code, changed 'long' to 'Long' for id field, and added newline at end of file."

--- padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -9,34 +9,35 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Cliente {
 
-    @Id 
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String nome;
-    @ManyToOne Endereco endereco;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	@ManyToOne
+	private Endereco endereco;
 
+	public Long getId() {
+		return id;
+	}
 
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public Endereco getEndereco() {
-        return endereco;
-    }
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
 
 }
