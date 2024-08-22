562703099675e8d1eead6fc828e7147a8ad2afcd
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 22:36:38 -0300

    "Removed duplicate import statement and reformatted code in ClienteRestController.java"

--- padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -11,42 +11,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.gof.model.Cliente;
 import one.digitalinnovation.gof.service.ClienteService;
-import one.digitalinnovation.gof.model.Cliente;;
 
 @RestController
 @RequestMapping("clientes")
 public class ClienteRestController {
 
-    @Autowired
-    private ClienteService clienteService;
+	@Autowired
+	private ClienteService clienteService;
 
-    @GetMapping
-    public ResponseEntity<Iterable<Cliente>> buscarTodos(){
-        return ResponseEntity.ok(clienteService.buscarTodos());
-    }
+	@GetMapping
+	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+		return ResponseEntity.ok(clienteService.buscarTodos());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
-        return ResponseEntity.ok(clienteService.buscarPorId(id));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+		return ResponseEntity.ok(clienteService.buscarPorId(id));
+	}
 
-    @PostMapping
-    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente){
-        clienteService.inserir(cliente);
-        return ResponseEntity.ok(cliente);
-    }
+	@PostMapping
+	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+		clienteService.inserir(cliente);
+		return ResponseEntity.ok(cliente);
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
-        clienteService.atualizar(id, cliente);
-        return ResponseEntity.ok(cliente);
-    }
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		clienteService.atualizar(id, cliente);
+		return ResponseEntity.ok(cliente);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable Long id){
-        clienteService.deletar(id);
-        return ResponseEntity.ok().build();
-    }
-    
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		clienteService.deletar(id);
+		return ResponseEntity.ok().build();
+	}
 }
