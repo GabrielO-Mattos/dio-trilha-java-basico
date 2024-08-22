a53a7132239b3e22284de317ea1e4c0d034cdf77
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-22 10:02:16 -0300

    "Added dependencies and implementation for endereco and viaCep services in ClienteServiceImpl"

--- padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/gof/service/impl/ClienteServiceImpl.java
@@ -3,15 +3,23 @@ package one.digitalinnovation.gof.service.impl;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import one.digitalinnovation.gof.model.Cliente;
 import one.digitalinnovation.gof.model.ClienteRepository;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.EnderecoRepository;
 import one.digitalinnovation.gof.service.ClienteService;
+import one.digitalinnovation.gof.service.ViaCepService;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
 
 
     @Override
@@ -37,7 +45,14 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public void inserir(Cliente cliente) {
-        
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);            
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 
     
