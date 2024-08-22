4adb4c940c2ad740b1e79845bc997fac3942bd6e
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-21 22:59:02 -0300

    Added `@EnableFeignClients` annotation to `PadroesProjectJavaSpringApplication` class.

--- padroes-project-java-spring/src/main/java/one/digitalinnovation/padroes_project_java_spring/PadroesProjectJavaSpringApplication.java
+++ padroes-project-java-spring/src/main/java/one/digitalinnovation/padroes_project_java_spring/PadroesProjectJavaSpringApplication.java
@@ -2,7 +2,9 @@ package one.digitalinnovation.padroes_project_java_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PadroesProjectJavaSpringApplication {
 
