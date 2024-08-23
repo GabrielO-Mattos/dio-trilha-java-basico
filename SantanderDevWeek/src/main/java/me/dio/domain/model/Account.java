6dcba279c4313aedc8bf31605728f5fe3a84eb65
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-23 11:17:57 -0300

    "Added JPA annotations to Account class"

--- SantanderDevWeek/src/main/java/me/dio/domain/model/Account.java
+++ SantanderDevWeek/src/main/java/me/dio/domain/model/Account.java
@@ -2,11 +2,23 @@ package me.dio.domain.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
     private String number;
     private String agency;
+    @Column(precision = 2, scale = 13)
     private BigDecimal balance;
+    @Column(precision = 2, scale = 13)
     private BigDecimal limit;
 
     public Long getId() {
