225e6123afa9e12724d02caad8613c2e71874486
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-23 11:24:33 -0300

    "Refactored Feature and News classes to extend BaseItem, removed redundant getters and setters"

--- SantanderDevWeek/src/main/java/me/dio/domain/model/News.java
+++ SantanderDevWeek/src/main/java/me/dio/domain/model/News.java
@@ -1,28 +1,5 @@
 package me.dio.domain.model;
 
-public class News {
+public class News extends BaseItem {
 
-    private Long id;
-    private String icon;
-    private String description;
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getIcon() {
-        return icon;
-    }
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
