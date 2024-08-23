0fc459e3f66f56f861472049dd65a600eaad519c
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-23 11:55:29 -0300

    "Added imports, annotations, and implemented methods in UserServiceImpl class"

--- SantanderDevWeek/src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ SantanderDevWeek/src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -1,7 +1,34 @@
 package me.dio.service.impl;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
 import me.dio.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User createUser(User userToCreate) {
+        if (userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())) {
+            throw new IllegalArgumentException("This user ID already exists!");
+        }
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
 
 }
