16669d3e79acbd3351d0c6cb7f1961521b893787
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-23 11:19:39 -0300

    "Added JPA annotations to Card class"

--- SantanderDevWeek/src/main/java/me/dio/domain/model/Card.java
+++ SantanderDevWeek/src/main/java/me/dio/domain/model/Card.java
@@ -2,10 +2,20 @@ package me.dio.domain.model;
 
 import java.math.BigDecimal;
 
-public class Card {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true)
     private String number;
+    @Column(precision = 2, scale = 13)
     private BigDecimal limit;
 
     
