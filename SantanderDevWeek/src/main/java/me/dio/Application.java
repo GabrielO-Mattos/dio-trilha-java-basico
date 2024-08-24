eb1150ada4920011f96dd207434754853923273f
Gabriel Oliveira de Matos <gabriel.sistemas@outlook.com>
2024-08-24 11:35:43 -0300

    "Added Dotenv configuration for PostgreSQL in Application.java and switched active profile to 'dev' in application.yml"

--- SantanderDevWeek/src/main/java/me/dio/Application.java
+++ SantanderDevWeek/src/main/java/me/dio/Application.java
@@ -3,10 +3,26 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		
+		String host = dotenv.get("PGHOST");
+		String port = dotenv.get("PGPORT");
+		String database = dotenv.get("PGDATABASE");
+		String user = dotenv.get("PGUSER");
+		String password = dotenv.get("PGPASSWORD");
+	
+		System.setProperty("PGHOST", host);
+		System.setProperty("PGPORT", port);
+		System.setProperty("PGDATABASE", database);
+		System.setProperty("PGUSER", user);
+		System.setProperty("PGPASSWORD", password);
+
 		SpringApplication.run(Application.class, args);
 	}
 
